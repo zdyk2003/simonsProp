176b6b82fa9dd7c93352b2119928a5059f3328c9
Zoe Dyksman <zbybee@hotmail.com>
2018-02-12 21:52:41 -0700

    simons files

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+ $(document).ready(function() {
+ 
+        $('#myCarousel').carousel({
+                interval: 4000
+        });
+ 
+        //Handles the carousel thumbnails
+        $('[id^=carousel-selector-]').click(function () {
+        var id_selector = $(this).attr("id");
+        try {
+            var id = /-(\d+)$/.exec(id_selector)[1];
+            console.log(id_selector, id);
+            jQuery('#myCarousel').carousel(parseInt(id));
+        } catch (e) {
+            console.log('error', e);
+        }
+    });
+        // When the carousel slides, auto update the text
+        $('#myCarousel').on('slid.bs.carousel', function (e) {
+                 var id = $('.item.active').data('slide-number');
+                $('#carousel-text').html($('#slide-content-'+id).html());
+        });
+});
